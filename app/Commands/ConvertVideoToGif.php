2980a043118d5ab0de85b9dba82144ae49b1d57f
Robbie Love <github@robbiecloud.com>
2023-08-06 19:15:50 +1000

    works with subtitles but with issues

--- app/Commands/ConvertVideoToGif.php
+++ app/Commands/ConvertVideoToGif.php
@@ -5,47 +5,149 @@ namespace App\Commands;
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
 
-class ConvertVideoToGif extends Command
+class ConvertVideoToGif1 extends Command
 {
-    protected $signature = 'convert {inputDirectory} {outputDirectory}';
-    protected $description = 'Convert video files to GIFs with overlaid subtitles.';
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:convert-video-to-gif1 {inputDir} {--outputDir=./output_gifs}';
 
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Convert videos in a directory to GIFs with subtitles';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
     public function handle()
     {
-        $inputDirectory = $this->argument('inputDirectory');
-        $outputDirectory = $this->argument('outputDirectory');
+        $inputDir = $this->argument('inputDir');
+        $outputDir = $this->option('outputDir');
 
-        $files = scandir($inputDirectory);
-        $videoFiles = preg_grep("/\.(mp4|mkv|avi|mov)$/i", $files);
-
-        foreach ($videoFiles as $videoFile) {
-            $this->info("Processing {$videoFile}...");
-
-            $filenameWithoutExtension = pathinfo($videoFile, PATHINFO_FILENAME);
-            $outputDirForCurrentFile = $outputDirectory . $filenameWithoutExtension;
-
-            if (file_exists($outputDirForCurrentFile)) {
-                array_map('unlink', glob("$outputDirForCurrentFile/*.gif"));
-            } else {
-                mkdir($outputDirForCurrentFile, 0777, true);
-            }
-
-            $subtitleFile = null;
-            foreach (["{$filenameWithoutExtension}.srt", "eng.srt", "en.srt", "english.srt"] as $potentialSubtitle) {
-                if (file_exists($inputDirectory . '/' . $potentialSubtitle)) {
-                    $subtitleFile = $potentialSubtitle;
-                    break;
-                }
-            }
-
-            $subtitleFilter = $subtitleFile ? "-vf subtitles='{$inputDirectory}/{$subtitleFile}'" : '';
-            $ffmpegCommand = "ffmpeg -i {$inputDirectory}/{$videoFile} $subtitleFilter {$outputDirForCurrentFile}/{$filenameWithoutExtension}.gif";
-
-            $this->info("Running command: $ffmpegCommand");
-            system($ffmpegCommand);
+        if (!is_dir($inputDir)) {
+            $this->error("Input directory {$inputDir} does not exist.");
+            return;
         }
 
-        $this->info("Processing finished.");
+        $videos = glob($inputDir . '/*.mp4');  // assuming mp4 format, adjust if needed
+
+        foreach ($videos as $video) {
+            $filename = pathinfo($video, PATHINFO_FILENAME);
+            $subtitleFiles = glob($inputDir . "/*.{srt,ass,ssa}", GLOB_BRACE);
+            $subtitleFile = $this->getSubtitleFile($filename, $subtitleFiles);
+
+            $targetDir = "{$outputDir}/{$filename}";
+            if (!file_exists($targetDir)) {
+                mkdir($targetDir, 0777, true);
+            } else {
+                array_map('unlink', glob("{$targetDir}/*.gif"));
+            }
+
+            $subtitles = [];
+            if ($subtitleFile) {
+                $subtitles = $this->parseSubtitles($subtitleFile);
+            }
+
+            if (!$subtitles) {
+                $videoDuration = shell_exec("ffmpeg -i {$video} 2>&1 | grep 'Duration' | cut -d ' ' -f 4 | sed s/,//");
+                $durationInSeconds = $this->convertToSeconds($videoDuration);
+                $subtitles = $this->generateDefaultSubtitles($durationInSeconds);
+            }
+
+            foreach ($subtitles as $index => $subtitle) {
+                $start = str_replace(',', '.', $subtitle['start']);
+                $end = str_replace(',', '.', $subtitle['end']);
+                $text = $subtitle['text'];
+
+                $outputGif = "{$targetDir}/{$filename}-{$index}.gif";
+                $this->info("Generating GIF for {$start} to {$end}");
+                $videoPath = escapeshellarg($video); // Escape the video path
+
+                // Clean up subtitle text by stripping HTML tags and other special characters
+                $cleanSubtitleText = escapeshellarg(html_entity_decode(strip_tags($subtitle['text'])));
+
+                $cmd = "ffmpeg -ss {$start} -to {$end} -i {$videoPath} -vf \"drawtext=text={$cleanSubtitleText}:x=(w-text_w)/2:y=h-th-10:fontsize=24:fontcolor=white\" -y {$outputDir}/{$filename}/{$filename}-{$index}.gif";
+
+                shell_exec($cmd);
+            }
+        }
+
+        $this->info("All videos processed!");
+    }
+
+    private function getSubtitleFile($videoFilename, $subtitleFiles) {
+        $preferredSubtitles = ['eng', 'en', 'english'];
+        $matchingSubtitles = array_filter($subtitleFiles, function ($subtitle) use ($videoFilename) {
+            return strpos($subtitle, $videoFilename) !== false;
+        });
+
+        if ($matchingSubtitles) {
+            return array_shift($matchingSubtitles);
+        }
+
+        foreach ($preferredSubtitles as $preferredSubtitle) {
+            foreach ($subtitleFiles as $subtitleFile) {
+                if (strpos($subtitleFile, $preferredSubtitle) !== false) {
+                    return $subtitleFile;
+                }
+            }
+        }
+
+        return $subtitleFiles[0] ?? null;
+    }
+
+    private function parseSubtitles($subtitleFile) {
+        $contents = file_get_contents($subtitleFile);
+        $lines = array_map('trim', explode("\n", $contents));
+
+        $subtitles = [];
+        $buffer = [];
+
+        foreach ($lines as $line) {
+            if (empty($line) && !empty($buffer)) {
+                if (preg_match("/(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})/", $buffer[1], $matches)) {
+                    $subtitles[] = [
+                        'start' => $matches[1],
+                        'end' => $matches[2],
+                        'text' => implode(' ', array_slice($buffer, 2))
+                    ];
+                }
+                $buffer = [];
+            } else {
+                $buffer[] = $line;
+            }
+        }
+
+        return $subtitles;
+    }
+
+    private function convertToSeconds($time) {
+        if (count(explode(":", $time)) !== 3) {
+            $this->error("Unexpected time format: " . $time);
+            return 0;
+        }
+        list($hours, $minutes, $seconds) = explode(":", $time);
+        return $hours * 3600 + $minutes * 60 + floatval($seconds);
+    }
+
+    private function generateDefaultSubtitles($duration) {
+        $subtitles = [];
+
+        for ($i = 0; $i < $duration; $i += 10) {
+            $subtitles[] = [
+                'start' => gmdate("H:i:s", $i),
+                'end' => gmdate("H:i:s", $i + 10),
+                'text' => ''
+            ];
+        }
+
+        return $subtitles;
     }
 }
-
